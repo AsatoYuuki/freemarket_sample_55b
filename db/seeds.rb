9eb66d4427869828be4d976d7c3204b91b794b42
AsatoYuuki <yuuki.ast@icloud.com>
2019-07-30 17:57:46 +0900

    コスメ、家電-uncestory

--- db/seeds.rb
+++ db/seeds.rb
@@ -117,3 +117,96 @@ men_underwear = men.children.create(name:"アンダーウェア")
 men_legwear.children.create([{name:"トランクス"}, {name:"ボクサーパンツ"},{name:"その他"}])
 
 men_etc = men.children.create(name:"その他")
+
+
+beauty = Category.create(name:"コスメ・香水・美容")
+beauty_basemakeup = beauty.children.create(name:"ベースメイク")
+beauty_basemakeup.children.create([{name:"ファンデーション"}, {name:"化粧下地"},{name:"コントロールカラー"},
+                                    {name:"BBクリーム"},{name:"CCクリーム"},{name:"コンシーラー"},{name:"フェイスパウダー"},{name:"トライアルセット/サンプル"},
+                                    {name:"その他"}])
+
+beauty_makeup = beauty.children.create(name:"メイクアップ")
+beauty_makeup.children.create([{name:"アイシャドウ"}, {name:"口紅"},{name:"リップグロス"},
+                                {name:"リップライナー"},{name:"チーク"},{name:"フェイスカラー"},{name:"マスカラ"},{name:"アイライナー"},
+                                {name:"つけまつげ"},{name:"アイブロウペンシル"},{name:"パウダーアイブロウ"},{name:"眉マスカラ"},
+                                {name:"トライアルセット/サンプル"},{name:"メイク道具/化粧小物"},{name:"美顔用品/美顔ローラー"},{name:"その他"}])
+
+beauty_nailcare = beauty.children.create(name:"ネイルケア")
+beauty_nailcare.children.create([{name:"ネイルカラー"}, {name:"カラージェル"},{name:"ネイルベースコート/トップコート"},
+                                  {name:"ネイルアート用品"},{name:"ネイルパーツ"},{name:"ネイルチップ/付け爪"},{name:"手入れ用具"},{name:"除光液"},
+                                  {name:"その他"}])
+
+beauty_perfume = beauty.children.create(name:"香水")
+beauty_perfume.children.create([{name:"香水（女性用）"}, {name:"香水（男性用）"},{name:"ユニセックス"},
+                                  {name:"ボディミスト"},{name:"その他"}])
+
+beauty_skincare = beauty.children.create(name:"スキンケア/基礎化粧品")
+beauty_skincare.children.create([{name:"化粧水/ローション"}, {name:"乳液/ミルク"},{name:"美容液"},
+                                  {name:"フェイスクリーム"},{name:"洗顔料"},{name:"クレンジング/メイク落とし"},{name:"パック/フェイスマスク"},{name:"ジェル/ゲル"},{name:"ブースター/導入液"},
+                                  {name:"アイケア"},{name:"リップケア"},{name:"トライアルセット/サンプル"},{name:"洗顔グッズ"},{name:"その他"}])
+
+beauty_haircare = beauty.children.create(name:"ヘアケア")
+beauty_haircare.children.create([{name:"シャンプー"}, {name:"トリートメント"},{name:"コンディショナー"},
+                                  {name:"リンス"},{name:"スタイリング剤"},{name:"カラーリング剤"},{name:"ブラシ"},{name:"その他"}])
+
+beauty_bodycare = beauty.children.create(name:"ボディケア")
+beauty_bodycare.children.create([{name:"オイル/クリーム"}, {name:"ハンドクリーム"},{name:"ローション"},
+                                  {name:"日焼け止め/サンオイル"},{name:"ボディソープ"},{name:"入浴剤"},{name:"制汗/デオドラント"},{name:"フットケア"},{name:"その他"}])
+
+beauty_oralcare = beauty.children.create(name:"オーラルケア")
+beauty_oralcare.children.create([{name:"口臭防止/エチケット用品"}, {name:"歯ブラシ"},{name:"その他"}])
+
+beauty_relaxation = beauty.children.create(name:"リラクゼーション")
+beauty_relaxation.children.create([{name:"エッセンシャルオイル"}, {name:"芳香剤"},{name:"お香/香炉"},
+                                    {name:"キャンドル"},{name:"リラクゼーショングッズ"},{name:"その他"}])
+
+beauty_diet = beauty.children.create(name:"ダイエット")
+beauty_diet.children.create([{name:"ダイエット食品"}, {name:"エクササイズ用品"},{name:"体重計"},
+                                    {name:"体脂肪計"},{name:"その他"}])
+                                  
+beauty_other = beauty.children.create(name:"その他")
+beauty_other.children.create([{name:"健康用品"}, {name:"看護/介護用品"},{name:"救急/衛星用品"},
+                                {name:"その他"}])
+
+
+electricappliances = Category.create(name:"家電・スマホ・カメラ")
+electricappliances_phone = electricappliances.children.create(name:"スマートフォン/携帯電話")
+electricappliances_phone.children.create([{name:"スマートフォン本体"}, {name:"バッテリー/充電器"},{name:"携帯電話本体"},
+                                    {name:"PHS本体"},{name:"その他"}])
+
+electricappliances_phoneaccessories = electricappliances.children.create(name:"スマホアクセサリー")
+electricappliances_phoneaccessories.children.create([{name:"Android用ケース"}, {name:"iPhone用ケース"},{name:"カバー"},
+                                    {name:"イヤホンジャック"},{name:"ストラップ"},{name:"フィルム"},{name:"自撮り棒"},{name:"その他"}])
+
+electricappliances_tablet = electricappliances.children.create(name:"PC/タブレット")
+electricappliances_tablet.children.create([{name:"タブレット"}, {name:"ノートPC"},{name:"デスクトップ型PC"},
+                                                      {name:"ディスプレイ"},{name:"電子ブックリーダー"},{name:"PC周辺機器"},{name:"PCパーツ"},{name:"その他"}])
+
+electricappliances_camera = electricappliances.children.create(name:"カメラ")
+electricappliances_camera.children.create([{name:"デジタルカメラ"}, {name:"ビデオカメラ"},{name:"レンズ（単焦点）"},
+                                                      {name:"レンズ（ズーム）"},{name:"フィルムカメラ"},{name:"防犯カメラ"},{name:"その他"}])
+
+electricappliances_TV = electricappliances.children.create(name:"テレビ/映像機器")
+electricappliances_TV.children.create([{name:"テレビ"}, {name:"プロジェクター"},{name:"ブルーレイレコーダー"},
+                                          {name:"DVDレコーダー"},{name:"ブルーレイプレイヤー"},{name:"DVDプレイヤー"},{name:"映像用ケーブル"},{name:"その他"}])
+
+electricappliances_audio = electricappliances.children.create(name:"オーディオ機器")
+electricappliances_audio.children.create([{name:"ポータブルプレーヤー"}, {name:"イヤフォン"},{name:"ヘッドフォン"},
+                                        {name:"アンプ"},{name:"スピーカー"},{name:"ケーブル/シールド"},{name:"ラジオ"},{name:"その他"}])
+
+electricappliances_beauty = electricappliances.children.create(name:"美容/健康")
+electricappliances_beauty.children.create([{name:"ヘアドライヤー"}, {name:"ヘアアイロン"},{name:"美容機器"},
+                                        {name:"電気シェーバー"},{name:"電動歯ブラシ"},{name:"その他"}])
+
+electricappliances_airconditioning = electricappliances.children.create(name:"冷暖房/空調")
+electricappliances_airconditioning.children.create([{name:"エアコン"}, {name:"空気清浄器"},{name:"加湿器"},
+                                                        {name:"扇風機"},{name:"除湿機"},{name:"ファンヒーター"},{name:"電気ヒーター"},{name:"オイルヒーター"},{name:"ストーブ"},
+                                                        {name:"ホットカーペット"},{name:"こたつ"},{name:"電気毛布"},{name:"その他"}])
+
+electricappliances_life = electricappliances.children.create(name:"生活家電")
+electricappliances_life.children.create([{name:"冷蔵庫"}, {name:"洗濯機"},{name:"炊飯器"},
+                                          {name:"電子レンジ/オーブン"},{name:"調理機器"},{name:"アイロン"},{name:"掃除機"},{name:"エスプレッソマシン"},{name:"コーヒーメーカー"},
+                                          {name:"衣類乾燥機"},{name:"その他"}])
+
+electricappliances_other = electricappliances.children.create(name:"その他")
+electricappliances_other.children.create([{name:"その他"}])
