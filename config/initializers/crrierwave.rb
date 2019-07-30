763ed8ccb8c3edae991c0e6b83639b3985b844d7
Yuki Sobue <yuki.sobue@gmail.com>
2019-07-30 16:02:49 +0900

    デプロイのエラーの箇所のための実験

--- config/initializers/crrierwave.rb
+++ config/initializers/crrierwave.rb
@@ -1,6 +1,6 @@
-# require 'carrierwave/storage/abstract'
-# require 'carrierwave/storage/file'
-# require 'carrierwave/storage/fog'
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
   if Rails.env.production?
@@ -10,8 +10,8 @@ CarrierWave.configure do |config|
     config.asset_host = 'https://s3-ap-northeast-1'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      # aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      # aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
       region:'ap-northeast-1',
       path_style: true
     }
