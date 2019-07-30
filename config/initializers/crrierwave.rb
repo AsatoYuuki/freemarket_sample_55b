743a8132f47f73e794d40b3a2d2f550a15fd4bca
Yuki Sobue <yuki.sobue@gmail.com>
2019-07-30 18:38:27 +0900

    バケットへのパスを修正しました

--- config/initializers/crrierwave.rb
+++ config/initializers/crrierwave.rb
@@ -7,7 +7,7 @@
 #     config.storage :fog
 #     config.fog_provider = 'fog/aws'
 #     config.fog_directory  = 'freemarket55b'
-#     config.asset_host = 'https://s3-ap-northeast-1'
+#     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket55b'
 #     config.fog_credentials = {
 #       provider: 'AWS',
 #       aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
