3fdb39a85c3046d7f2bb51e4047e2e76b1129823
Yuki Sobue <yuki.sobue@gmail.com>
2019-08-02 12:10:13 +0900

    add search_controller.rb
    複数語で検索をするためのコントローラの追加

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,11 @@
 class SearchController < ApplicationController
   def index
+    keywords = params[:search]&.split(/[[:blank:]]+/)
+    @items = []
+    keywords&.each do |keyword| 
+      next if keyword == "" 
+      @items += Item.where('name LIKE(?)', "%#{keyword}%") 
+    end 
+    @items.uniq! 
   end
 end
