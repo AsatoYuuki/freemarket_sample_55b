cde8503bd9fcccc28cb613029119a492d0c34903
粟野未早希 <awanomisaki@awanomisakinoMacBook-Air.local>
2019-08-12 10:19:19 +0900

    Update cards_contoroller.rb
    payjpのカード登録・削除用アクション追加

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,7 +1,57 @@
 class CardsController < ApplicationController
-  def show
+  
+  require "payjp"
+
+  def new
+    card = Card.where(user_id: current_user.id)
+    if card.exists?
+      redirect_to mypage_cards_path
+    else
+      @card = Card.new
+    end
   end
 
-  def create
+  def pay #payjpとCardのデータベース作成を実施します。
+    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+    if params['payjp-token'].blank?
+      redirect_to action: "new"
+    else
+      customer = Payjp::Customer.create(
+      card: params['payjp-token'],
+      metadata: {user_id: current_user.id}
+      ) 
+      @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      if @card.save
+        redirect_to mypage_cards_path
+      else
+        redirect_to action: "pay"
+      end
+    end
   end
+
+  def delete
+    @card = Card.where(user_id: current_user.id).first
+    if @card.blank?
+    else
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      customer.delete
+      if @card.delete
+        redirect_to mypage_cards_path
+      end
+    end
+  end
+
+  
+  def show
+    @card = Card.where(user_id: current_user.id).first
+    if @card.blank?
+      redirect_to action: "new" 
+    else
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card_information = customer.cards.retrieve(@card.card_id)
+    end
+  end
+
 end
