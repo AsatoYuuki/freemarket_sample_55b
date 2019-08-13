dc11184c92eb00f7c4d5560b1b6ce8015b73bb80
粟野未早希 <awanomisaki@awanomisakinoMacBook-Air.local>
2019-08-13 16:59:01 +0900

    Update user.rb
    不要なバリデーション削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,5 +20,4 @@ class User < ApplicationRecord
   validates :prefecture, {presence: true}
   validates :city, {presence: true}
   validates :address, {presence: true}
-  validates :phone_number,numericality:{only_integer: true}
 end
