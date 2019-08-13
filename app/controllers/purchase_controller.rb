4140e479aca6489f1a80e814225afbdd5ce14cf8
粟野未早希 <awanomisaki@awanomisakinoMacBook-Air.local>
2019-08-13 11:19:54 +0900

    Create purchase_controller.rb
    商品購入用コントローラの生成

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -3,6 +3,9 @@ class PurchaseController < ApplicationController
   require 'payjp'
 
   def show
+    @item = Item.find(params[:id])
+    # @user = 
+    
     card = Card.where(user_id: current_user.id).first
     if card.blank?
       redirect_to controller: "card", action: "new"
@@ -14,17 +17,38 @@ class PurchaseController < ApplicationController
   end
 
   def pay
+    item = Item.find(params[:item_id])
+    price = item.price
     card = Card.where(user_id: current_user.id).first
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     Payjp::Charge.create(
-    amount: 500, #支払金額を入力（itemテーブル等に紐づけても良い）
+    amount: price, #支払金額を入力（itemテーブル等に紐づけても良い）
     customer: card.customer_id, #顧客ID
     currency: 'jpy', #日本円
   )
+  item.update(buyer_id:"1")
   redirect_to action: 'done' 
   end
 
 
   def done
+  end
+end
+
+def pay #payjpとCardのデータベース作成を実施します。
+  Payjp.api_key = 'sk_test_a17c0589014ff4e26b8c6d4b'
+  if params['payjp-token'].blank?
+    redirect_to action: "new"
+  else
+    customer = Payjp::Customer.create(
+    card: params['payjp-token'],
+    metadata: {user_id: current_user.id}
+    )
+    @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      if @card.save
+        redirect_to users_thanks_path
+      else
+        redirect_to action: "pay"
+    end
   end
 end
