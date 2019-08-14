b2d88bbfad30bd2c1430149d7f35b48ccf04a6f6
sui625 <jun69u@icloud.com>
2019-08-14 18:23:08 +0900

    validate

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,4 +2,6 @@ class Image < ApplicationRecord
   mount_uploader :url, ImageUploader
 
   belongs_to :item, optional: true
+
+  validates :url, presence: true
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,7 @@ class Item < ApplicationRecord
 
   accepts_nested_attributes_for :images
 
+  validates_associated :images
+  validates :price, numericality: { only_integer: true }
+  validates :name, :content, :condition, :send_cost, :send_method, :send_place, :send_day, :category_id, presence: {message: "空欄を埋めてください"}
 end
